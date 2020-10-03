538d3c367102bfc120c95c0beff4d7a45cd85be9
Megha <megha.banerjee07@gmail.com>
2020-10-03 20:16:08 +0530

    'digitalcalculator'

---
+++ js/script.js
@@ -0,0 +1,39 @@
+function inputEntered(event) {
+    let inputValue = document.querySelector('.calculator-data-input').value;
+    let enteredValue = event.target.value;
+    let calcValue = inputValue.concat(enteredValue);
+    document.querySelector('.calculator-data-input').value = calcValue;
+}
+
+function clearAll(event) {
+    document.querySelector('.calculator-data-input').value = "";
+}
+
+function deleteLastInput(event) {
+    let inputValue = document.querySelector('.calculator-data-input').value;
+    let deleteOutput = inputValue.slice(0, -1);
+    document.querySelector('.calculator-data-input').value = deleteOutput;
+}
+
+function evaluteResult(event) {
+    inputEntered(event);
+    let inputValue = document.querySelector('.calculator-data-input').value;
+    let deleteOutput = inputValue.slice(0, -1);
+    let evalResult;
+
+    if (deleteOutput.includes('^')) {
+        let powerIndex = deleteOutput.indexOf('^');
+        let num1 = deleteOutput.slice(0, powerIndex);
+        let num2 = deleteOutput.slice(powerIndex + 1, deleteOutput.length);
+
+        evalResult = Math.pow(num1, num2);
+
+    } else {
+        evalResult = eval(deleteOutput);
+        console.log(evalResult);
+    }
+
+    let evalResultDisplay = inputValue.concat(evalResult);
+    document.querySelector('.calculator-data-input').value = evalResultDisplay;
+
+}
